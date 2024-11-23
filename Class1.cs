d281b53a0bc62ce4420cff7dadaa7b670ed79ef9
Satish Chandra Karki <satishh051@gmail.com>
2024-11-23 20:40:57 +0545

    assignment-1

--- Class1.cs
+++ Class1.cs
@@ -1,24 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-class Program
+namespace Basic_assignment
 {
-    static void Main()
+    internal class Program
     {
-        // Initialize two variables
-        int a = 5;
-        int b = 10;
+        static void Main(string[] args)
+        {
+            //Swapping values of two variables without using the third variable
+            int first_var = 5;
+            int second_var = 10;
+            Console.WriteLine("Before Swapping:");
+            Console.WriteLine("First_var=" + first_var);
+            Console.WriteLine("Second_var=" + second_var);
+            first_var = first_var + second_var;
+            second_var = first_var - second_var;
+            first_var = first_var - second_var;
+            Console.WriteLine("\nAfter Swapping:");
+            Console.WriteLine("First_var=" + first_var);
+            Console.WriteLine("Second_var=" + second_var);
 
-        Console.WriteLine("Before swapping:");
-        Console.WriteLine($"a = {a}");
-        Console.WriteLine($"b = {b}");
 
-        // Swap using a temporary variable
-        int temp = a;
-        a = b;
-        b = temp;
-
-        Console.WriteLine("\nAfter swapping:");
-        Console.WriteLine($"a = {a}");
-        Console.WriteLine($"b = {b}");
+        }
     }
 }
+
