df2525d08790232cd474c2d4963a68b67ab97100
Satish Chandra Karki <satishh051@gmail.com>
2024-11-23 14:12:06 +0545

    one

--- Class1.cs
+++ Class1.cs
@@ -1,8 +1,24 @@
 ﻿using System;
 
-public class Class1
+class Program
 {
-	public Class1()
-	{
-	}
+    static void Main()
+    {
+        // Initialize two variables
+        int a = 5;
+        int b = 10;
+
+        Console.WriteLine("Before swapping:");
+        Console.WriteLine($"a = {a}");
+        Console.WriteLine($"b = {b}");
+
+        // Swap using a temporary variable
+        int temp = a;
+        a = b;
+        b = temp;
+
+        Console.WriteLine("\nAfter swapping:");
+        Console.WriteLine($"a = {a}");
+        Console.WriteLine($"b = {b}");
+    }
 }
