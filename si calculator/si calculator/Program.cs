27a6cf89d12dd0af8f64c004a91d280ab7518dce
Satish Chandra Karki <satishh051@gmail.com>
2024-12-08 08:42:35 +0545

    added calculator

---
+++ si calculator/si calculator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SimpleInterestApp
+{
+    // SimpleInterestCalculator class
+    public class SimpleInterestCalculator
+    {
+        // Method to calculate simple interest
+        public double CalculateInterest(double principal, double rate, double time)
+        {
+            return (principal * rate * time) / 100;
+        }
+    }
+
+    // Main program
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an object of SimpleInterestCalculator
+            SimpleInterestCalculator calculator = new SimpleInterestCalculator();
+
+            // Input: Accept user input for principal, rate, and time
+            Console.WriteLine("Enter Principal Amount:");
+            double principal = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Rate of Interest:");
+            double rate = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Time (in years):");
+            double time = double.Parse(Console.ReadLine());
+
+            // Calculate and display the simple interest
+            double interest = calculator.CalculateInterest(principal, rate, time);
+            Console.WriteLine($"The Simple Interest is: {interest}");
+        }
+    }
+}
